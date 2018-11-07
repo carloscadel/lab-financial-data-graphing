73abdf847c69ed833ee1f2524b380581d35846c7
carloscadel <c.cazallas.delfa@gmail.com>
2018-11-07 19:37:54 +0100

    Iterations 1 and 2 done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,49 @@
+// ?index=[USD/CNY] The index to return data for. Defaults to USD.
+// ?currency=<VALUE>The currency to return the data in, specified in ISO 4217 format. Defaults to USD.
+// ?start=<VALUE>&end=<VALUE> Allows data to be returned for a specific date range. Must be listed as a pair of start and end parameters, with dates supplied in the YYYY-MM-DD format, e.g. 2013-09-01 for September 1st, 2013.
+// ?for=yesterdaySpecifying this will return a single value for the previous day. Overrides the start/end parameter.
+
+const coinApi = axios.create({
+//   baseURL: ('https://api.coindesk.com/v1/bpi/historical/close.json' + "?start=" + dateFrom + "&end=" + dateUntil)
+  baseURL: ('https://api.coindesk.com/v1/bpi/historical/close.json')
+})
+
+function getData(dateFrom, dateUntil) {
+    let parameters = "?start=" + dateFrom + "&end=" + dateUntil
+    coinApi.get(parameters)
+      .then(response => {
+        let obj = response.data.bpi
+        const arr = Object.entries(obj)
+        printTheChart(arr)
+        // console.log(response.data.bpi)
+        console.log(arr)
+        // console.log(arr[0])
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+function printTheChart(stockData) {
+    let stockLabels = stockData.map( element => element[0]);
+    let stockPrice = stockData.map( element => element[1]);
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+        labels: stockLabels,
+        datasets: [{
+            label: "Stock Chart",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: stockPrice,
+        }]
+        }
+    });
+};
+
+document.getElementById("coinButton").onclick = function () {
+    const dateFrom = document.getElementById("date-from").value
+    const dateUntil = document.getElementById("date-until").value
+    getData(dateFrom, dateUntil);
+}
+
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
+
+const coinApi = axios.create({
+  baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
